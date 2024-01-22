3168026072dd8465dff7ce79fb945cdc20544cfe
Fiegsy <yassine.daoui031@gmail.com>
2024-01-22 13:22:45 -0800

    Fixed code implemenattion

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 += ' ' + str2
-print(f"Welcome to {str1}!")
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
