7d795a553e143024330593f218da50963f5ec202
Fiegsy <yassine.daoui031@gmail.com>
2024-01-24 06:40:45 -0800

    fixed missing return last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        last_digit = (number % -10) * -1
-    if number >= 0:
-        last_digit = (number % 10)
-    print("{}".format(last_digit), end="")
-    return (last_digit)
+        last_digit = number % -10 * -1
+    else:
+        last_digit = number % 10
+
+    print(last_digit, end="")
+    return last_digit
