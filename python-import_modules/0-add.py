dc3db32212a5fbb3e0296fdd5bfbfb6e4ad25014
Fiegsy <yassine.daoui031@gmail.com>
2024-01-26 06:16:32 -0800

    Fixed indention

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
+if __name__ == "__main__":
+    from add_0 import add as add
 a = 1
 b = 2
-
-if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
