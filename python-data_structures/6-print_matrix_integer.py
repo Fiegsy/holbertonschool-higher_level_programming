71277a9ebae76015679f04a74d5cbb2b81dc2e36
Fiegsy <yassine.daoui031@gmail.com>
2024-01-30 08:33:43 -0800

    Fixed ind

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=None):
+    if matrix is None:
+        return None
+    
     for row in matrix:
         print(" ".join(map(str, row)))
