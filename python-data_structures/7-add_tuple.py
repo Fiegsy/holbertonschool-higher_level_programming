79b8040f5ae97ee79fca47c975646f9e6fb63f5c
Fiegsy <yassine.daoui031@gmail.com>
2024-01-30 08:38:59 -0800

    Fixed indt

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(0,), tuple_b=(0,)):
-    ele1 = tuple_a[0] + tuple_b[0]
-    ele2 = tuple_a[1] + tuple_b[1]
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = (tuple_a[0] if len(tuple_a) > 0 else 0, tuple_a[1] if len(tuple_a) > 1 else 0)
+    
+    if len(tuple_b) < 2:
+        tuple_b = (tuple_b[0] if len(tuple_b) > 0 else 0, tuple_b[1] if len(tuple_b) > 1 else 0)
 
-    return (ele1, ele2)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
