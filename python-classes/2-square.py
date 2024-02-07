ca60eb384a5eed328197f6ecfd32860519d86046
Fiegsy <yassine.daoui031@gmail.com>
2024-02-07 15:11:14 -0800

    fixed indt

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,71 +1,36 @@
 #!/usr/bin/python3
-"""Module with a Square class containing attributes"""
+"""empty module"""
 
 
 class Square:
-    """Class to define a square of a given size"""
-    def __init__(self, size=0, color='white', filled=True):
-        """Initialize a square to a given size.
+    """empty class"""
+    def __init__(self, size=0):
+        """Initialize a square with a given size.
 
         Args:
-            size (int, optional): Size of the square.
-                Defaults to 0. Stored as a private attribute
-            color (str, optional): Color of the square.
-                Defaults to 'white'. Stored as a private attribute
-            filled (bool, optional): Whether the square is filled.
-                Defaults to True. Stored as a private attribute
+            size (int, optional): Size of the square. Defaults to 0.
 
         Raises:
-            TypeError: If the size attribute is not an integer.
-            ValueError: If the size attribute is less than zero.
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        self.__color = color
-        self.__filled = filled
+        self.__size = size
 
     @property
     def size(self):
-        """Get the size of the square."""
+        """Getter method for size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square.
-
-        The value must be an integer greater than or equal to 0.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than 0.
-        """
+        """Setter method for size."""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def color(self):
-        """Get the color of the square."""
-        return self.__color
-
-    @color.setter
-    def color(self, value):
-        """Set the color of the square."""
-        self.__color = value
-
-    @property
-    def filled(self):
-        """Check if the square is filled."""
-        return self.__filled
-
-    @filled.setter
-    def filled(self, value):
-        """Set whether the square is filled."""
-        self.__filled = value
+        
