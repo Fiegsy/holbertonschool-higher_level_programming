10bd724b4e39ececcb486f9501f64d144fe4b112
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 03:25:03 -0800

    Fixed dt file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,17 @@
 def say_my_name(first_name, last_name=""):
-    """Prints first and last names.
+    """Prints out the name and last name passed as arguments in a sentence.
 
     Args:
         first_name (str): The first name.
         last_name (str, optional): The last name. Defaults to "".
 
     Raises:
-        TypeError: If first_name or last_name is not a string.
+        TypeError: If first_name is not a string or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print("My name is {} {}".format(first_name, last_name))
