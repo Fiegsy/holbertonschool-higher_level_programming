21b07c885138773304b7a623c2473982f6114779
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 03:19:34 -0800

    fixed indent

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,40 @@
-#!/usr/bin/python3
-"""module  contains say_my_name"""
-
-
 def say_my_name(first_name, last_name=""):
-    """Fonction print first and last name"""
+    """Print out the name and last name passed as arguments in a sentence.
+
+    Args:
+        first_name (string): First name to print.
+        last_name (string, optional): Last name to print. Defaults to "".
+
+    Raises:
+        TypeError: if either of the two arguments is not a string.
+    """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+
+    print(f"My name is {first_name} {last_name}")
+
+
+if __name__ == "__main__":
+    try:
+        say_my_name("John", "Doe")
+    except Exception as e:
+        print(e)
+
+    try:
+        say_my_name("Jane")
+    except Exception as e:
+        print(e)
+
+    try:
+        say_my_name(123, "Smith")
+    except Exception as e:
+        print(e)
+
+    try:
+        say_my_name("Alex", 456)
+    except Exception as e:
+        print(e)
