a06956bd6490cde36c20fe863c303c1de7da03dc
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 02:52:22 -0800

    fixed indt

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,16 @@
 def say_my_name(first_name, last_name=""):
-    """Function to print first and last name."""
+    """Function to print first and last name.
+
+    Args:
+        first_name (str): The first name of the person.
+        last_name (str, optional): The last name of the person. Defaults to "".
+
+    Raises:
+        TypeError: If first_name is not a string.
+        TypeError: If last_name is not a string.
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
