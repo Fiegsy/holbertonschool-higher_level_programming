063f2649e4eaab428baab4b1903e48b463248149
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 03:08:53 -0800

    fixed indt

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -13,4 +13,13 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+
     print(f"My name is {first_name} {last_name}")
+
+# Test cases
+try:
+    say_my_name("John", "Smith")
+    say_my_name("Walter", "White")
+    say_my_name("Bob")
+except TypeError as e:
+    print(e)
