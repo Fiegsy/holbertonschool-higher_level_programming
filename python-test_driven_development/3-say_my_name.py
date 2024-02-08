98d5d6705b09f4d9dd58af75b9afe2e8eb4430f1
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 06:33:17 -0800

    fixed indt

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""Module with a function that prints out its arguments in a sentence"""
+"""module contains say_my_name"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print out the name and last name passed as arguments in a sentence.
-
-    Args:
-        first_name (string): First name to print.
-        last_name (string, optional): Last name to print. Defaults to "".
-
-    Raises:
-        TypeError: if either of the two arguments is not a string.
-    """
+    """Function to print first and last name"""
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print(f"My name is {first_name} {last_name}")
+    print("My name is {} {}".format(first_name, last_name))
