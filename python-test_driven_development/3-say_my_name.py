1feb6f5f7fc32d431b45a0e2d138ed4aff735313
Fiegsy <yassine.daoui031@gmail.com>
2024-02-08 03:12:34 -0800

    fixed indt files

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,12 @@
+#!/usr/bin/python3
+"""module  contains say_my_name"""
+
+
 def say_my_name(first_name, last_name=""):
-    """Function to print first and last name.
+    """Fonction print first and last name"""
 
-    Args:
-        first_name (str): The first name of the person.
-        last_name (str, optional): The last name of the person. Defaults to "".
-
-    Raises:
-        TypeError: If first_name is not a string.
-        TypeError: If last_name is not a string.
-    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print(f"My name is {first_name} {last_name}")
-
-# Test cases
-try:
-    say_my_name("John", "Smith")
-    say_my_name("Walter", "White")
-    say_my_name("Bob")
-except TypeError as e:
-    print(e)
+    print("My name is {} {}".format(first_name, last_name))
