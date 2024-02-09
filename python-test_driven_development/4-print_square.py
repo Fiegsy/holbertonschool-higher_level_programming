b9418995104630ddde3c5efec07d458176a62894
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 03:19:46 -0800

    task 3 file fixed idt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,52 +1,24 @@
 #!/usr/bin/python3
-"""Module that prints a square."""
+"""Module with a function that prints out a square of '#'s"""
 
 
 def print_square(size):
-    """Prints a square of a given size."""
-    if not isinstance(size, int):
+    """Print out a square with the char '#' of the size determined by the
+    'size' argument.
+
+    Args:
+        size (int): Positive integer representing the length of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than zero.
+    """
+
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size < 0:
+
+    elif size < 0:
         raise ValueError("size must be >= 0")
-    for _ in range(size):
+
+    for i in range(size):
         print("#" * size)
-
-
-# Test cases
-if __name__ == "__main__":
-    try:
-        print_square(-1)
-    except ValueError as ve:
-        print(ve)
-    
-    try:
-        print_square('1')
-    except TypeError as te:
-        print(te)
-    
-    try:
-        print_square(-1.5)
-    except TypeError as te:
-        print(te)
-    
-    try:
-        print_square(None)
-    except TypeError as te:
-        print(te)
-    
-    try:
-        print_square(["two"])
-    except TypeError as te:
-        print(te)
-    
-    try:
-        print_square(1, 2)
-    except TypeError as te:
-        print(te)
-    
-    try:
-        print_square()
-    except TypeError as te:
-        print(te)
-    
-    print_square(0)
