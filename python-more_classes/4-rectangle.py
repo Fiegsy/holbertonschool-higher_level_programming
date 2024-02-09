8aef7fc6bbaf724aaa703daaf6c54be92bec23ce
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 05:23:15 -0800

    Task 4 file

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""empty module"""
+
+
+class Rectangle:
+    """a class create rectangle"""
+    def __init__(self, width=0, height=0):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height * 2) + (self.__width * 2)
+
+    def __str__(self):
+        str_rectangle = ''
+        if self.__height != 0 and self.__width != 0:
+            for i in range(self.__height):
+                if i == self.__height - 1:
+                    str_rectangle += '#' * self.__width
+                else:
+                    str_rectangle += '#' * self.__width + '\n'
+        return str_rectangle
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
