83ed61b280293238724ced8037bb711dd940cf14
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 05:51:49 -0800

    Fixed idt

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""Unittest for min_integer([..])
+"""Unittest for max_integer([..])
 """
 
 
 import unittest
-min_integer = __import__('6-min_integer').min_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
-class TestMinInteger(unittest.TestCase):
-    """The Unittest for min integer function"""
+class TestMaxInteger(unittest.TestCase):
+    """The Unittest for max integer function"""
 
-    def test_min_integer(self):
-        self.assertEqual(min_integer([1, 2, 3, 4]), 1)
-        self.assertEqual(min_integer([1, 3, 4, 2]), 1)
-        self.assertEqual(min_integer([]), None)
-        self.assertEqual(min_integer([-1, -2, -3, -4]), -4)
-        self.assertEqual(min_integer([1]), 1)
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([1]), 1)
 
     def test_notListArg(self):
         with self.assertRaises(TypeError):
-            min_integer(27)
+            max_integer(27)
 
     def test_ListWithNotInt(self):
         with self.assertRaises(TypeError):
-            min_integer([1, 2, 3, "10"])
+            max_integer([1, 2, 3, "10"])
     
     def test_NoArg(self):
-        self.assertEqual(min_integer(), None)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        self.assertEqual(max_integer(), None)
