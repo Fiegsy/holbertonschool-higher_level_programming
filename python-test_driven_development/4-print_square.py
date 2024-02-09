2a0a822ae12bf48583f3d48d257cea806d00cebf
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 05:39:12 -0800

    Fixed indt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
-"""module that print square"""
+"""Module with a function that prints out a square of '#'s"""
 
 
 def print_square(size):
-    """a function print a square"""
-    if not isinstance(size, int):
+    """Print out a square with the char '#' of the size determined by the
+    'size' argument.
+
+    Args:
+        size (int): Positive integer representing the length of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than zero.
+    """
+
+    if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size < 0:
+
+    elif size < 0:
         raise ValueError("size must be >= 0")
+
     for i in range(size):
-        print(size * '#')
+        print("#" * size)
