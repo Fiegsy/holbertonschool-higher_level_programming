64f807cc9bf0d2c9051e316921363fa2e04c0b13
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 03:33:54 -0800

    Fixed indt

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,69 +1,23 @@
 #!/usr/bin/python3
-"""Module for text indentation."""
+"""a module that defines the text_identation function"""
 
 
 def text_indentation(text):
-    """Indent a text by adding two new lines after specific separators.
+    """a function that print the content of a text
 
-    Args:
-        text (str): Text to indent
+    returns: nothing
 
-    Raises:
-        TypeError: if text is not a string.
+    raises:
+        TypeError: if the text is not a string
     """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
-    separators = ".?:"
-    indented_text = ""
-    start_index = 0
-
-    for index, char in enumerate(text):
-        if char in separators or index == len(text) - 1:
-            substring = text[start_index:index + 1].strip()
-            indented_text += substring + "\n\n"
-            start_index = index + 1 if char == " " else index
-
-    print(indented_text.strip(), end="")
-
-
-# Test cases
-if __name__ == "__main__":
-    try:
-        text_indentation("Bonjour. Comment allez-vous? Dites cela: Hej.")
-    except Exception as e:
-        print(e)
-
-    try:
-        text_indentation(22)
-    except Exception as e:
-        print(e)
-
-    try:
-        text_indentation("")
-    except Exception as e:
-        print(e)
-
-    try:
-        text_indentation(
-            """Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-            Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-            Non autem hoc: igitur ne illud quidem."""
-        )
-    except Exception as e:
-        print(e)
-
-    try:
-        text_indentation()
-    except Exception as e:
-        print(e)
-
-    try:
-        text_indentation(
-            """         Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-            Quonam modo?                   Utrum igitur tibi litteram videor an totas paginas commovere? \
-            Non autem hoc: igitur ne illud quidem."""
-        )
-    except Exception as e:
-        print(e)
+    sentence = ""
+    for i in text:
+        sentence += i
+        if i in [".", "?", ":"]:
+            print("{}\n".format(sentence.strip()))
+            sentence = ""
+    if sentence.strip() != "":
+        print("{}".format(sentence.strip()), end="")
