9f6fe10e7715d7d6710b181d72a58958292b8e4f
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 05:50:56 -0800

    Fixed indt

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,57 +1,32 @@
 #!/usr/bin/python3
-"""Unittest for Rectangle class
+"""Unittest for min_integer([..])
 """
 
 
 import unittest
-from rectangle import Rectangle
+min_integer = __import__('6-min_integer').min_integer
 
 
-class TestRectangle(unittest.TestCase):
-    """Unittest for Rectangle class"""
+class TestMinInteger(unittest.TestCase):
+    """The Unittest for min integer function"""
 
-    def test_area(self):
-        rect = Rectangle(4, 5)
-        self.assertEqual(rect.area(), 20)
+    def test_min_integer(self):
+        self.assertEqual(min_integer([1, 2, 3, 4]), 1)
+        self.assertEqual(min_integer([1, 3, 4, 2]), 1)
+        self.assertEqual(min_integer([]), None)
+        self.assertEqual(min_integer([-1, -2, -3, -4]), -4)
+        self.assertEqual(min_integer([1]), 1)
 
-    def test_perimeter(self):
-        rect = Rectangle(4, 5)
-        self.assertEqual(rect.perimeter(), 18)
+    def test_notListArg(self):
+        with self.assertRaises(TypeError):
+            min_integer(27)
 
-    def test_str(self):
-        rect = Rectangle(4, 5)
-        self.assertEqual(str(rect), "####\n####\n####\n####\n####")
-
-    def test_repr(self):
-        rect = Rectangle(4, 5)
-        self.assertEqual(repr(rect), "Rectangle(4, 5)")
-
-    def test_set_width(self):
-        rect = Rectangle(4, 5)
-        rect.width = 6
-        self.assertEqual(rect.width, 6)
-
-    def test_set_height(self):
-        rect = Rectangle(4, 5)
-        rect.height = 6
-        self.assertEqual(rect.height, 6)
-
-    def test_get_number_of_instances(self):
-        rect1 = Rectangle(4, 5)
-        rect2 = Rectangle(2, 3)
-        self.assertEqual(rect1.get_number_of_instances(), 2)
-
-    def test_bigger_or_equal(self):
-        rect1 = Rectangle(4, 5)
-        rect2 = Rectangle(2, 3)
-        self.assertEqual(Rectangle.bigger_or_equal(rect1, rect2), rect1)
-        rect3 = Rectangle(6, 7)
-        self.assertEqual(Rectangle.bigger_or_equal(rect3, rect1), rect3)
-
-    def test_square(self):
-        square = Rectangle.square(5)
-        self.assertEqual(square.width, 5)
-        self.assertEqual(square.height, 5)
+    def test_ListWithNotInt(self):
+        with self.assertRaises(TypeError):
+            min_integer([1, 2, 3, "10"])
+    
+    def test_NoArg(self):
+        self.assertEqual(min_integer(), None)
 
 
 if __name__ == "__main__":
