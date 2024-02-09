f437e999040de631db408ea04bac1a83620aa0a4
Fiegsy <yassine.daoui031@gmail.com>
2024-02-09 05:37:19 -0800

    Fixed implem

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-"""Module with a function that prints out a square of '#'s"""
+"""module that print square"""
 
 
 def print_square(size):
-    """Print out a square with the char '#' of the size determined by the
-    'size' argument.
-
-    Args:
-        size (int): Positive integer representing the length of the square.
-
-    Raises:
-        TypeError: if size is not an integer.
-        ValueError: if size is less than zero.
-    """
-
-    if type(size) is not int:
+    """a function print a square"""
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
-    elif size < 0:
+    if size < 0:
         raise ValueError("size must be >= 0")
-
     for i in range(size):
-        print("#" * size)
+        print(size * '#')
