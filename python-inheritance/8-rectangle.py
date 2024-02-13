894dc6b8baba74ef90df426cadb1b476170beadc
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 04:02:16 -0800

    Fixed indt

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(__import__('7-base_geometry').BaseGeometry):
 
     def __init__(self, width, height):
         """Initializes a Rectangle instance with the given width and height"""
+        super().__init__()
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        
