b0cbd9c945d76eff4965736323b110fc03377e0a
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 03:32:06 -0800

    Fixed indt

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,6 +5,6 @@
 class MyList(list):
     """a function that inherits a list"""
     def print_sorted(self):
-        """Prints the sorted list"""
-        sorted_list = sorted(self)
-        print("Sorted list:", sorted_list)
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
