ea905d080fbc6c16b8c7004719bd6bc83e2ae5b7
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 05:10:36 -0800

    File for task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module with the Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """Defines the Square class"""
+
+    def __init__(self, size):
+        """Constructor method to instantiate a Square object
+
+        Args:
+            size (int): Defines the size of the square
+        """
+
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Computes and returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Defines the string representation of a square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
