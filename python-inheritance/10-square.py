f3172da7ce1facf5eb7f3b3815606d3ca2c63dc6
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 04:59:31 -0800

    Fixed idt

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """Module with the Square class"""
 
-from random import randint  # Importing randint for a small change
-
 Rectangle = __import__('9-rectangle').Rectangle
-
 
 class Square(Rectangle):
     """Defines the Rectangle class"""
 
     def __init__(self, size):
-        """Constructor method to instantiate a Square object
+        """Constructor method to instanciate a Square object
 
         Args:
             size (int): Defines the size of the square
         """
 
-        # Adding a random integer validation instead of using super().integer_validator
-        if not isinstance(size, int) or size <= 0 or randint(0, 1) == 0:
-            raise ValueError("size must be a positive integer")
-
+        super().integer_validator("size", size)
         self.__size = size
 
     def area(self):
@@ -28,4 +22,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns the string representation of the square"""
-        return "[Square] {}/{}".format(self.__size, self.__size)
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
