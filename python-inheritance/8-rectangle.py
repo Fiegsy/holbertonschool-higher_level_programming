ce0c34a214c58901349c66ac095bc69b554aa485
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 04:05:48 -0800

    Idt fixed

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Geometry module"""
+'''A module that contains Rectangle class'''
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """a class inheriting from the basegeometry class"""
+    '''a  Class that creates a rectangle'''
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-        
