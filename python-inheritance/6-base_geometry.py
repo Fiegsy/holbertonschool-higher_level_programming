fa562a528933f911383f66f9e74a74dacd64946e
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 03:54:03 -0800

    Task 6 file

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """A BaseGeometry class"""
+    def area(self):
+        """Raises an exception"""
+        raise NotImplementedError("area() is not implemented")
