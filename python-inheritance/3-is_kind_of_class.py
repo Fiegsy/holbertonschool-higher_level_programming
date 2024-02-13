44e52118852d8c59fee3f9efff73e6f2b0c197eb
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 03:45:17 -0800

    Task 3 file

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function to check if an object is an instance of a class"""
+
+
+def is_instance_of_class(obj, a_class):
+    """Function that returns True if the object is an instance of the specified class"""
+    return isinstance(obj, a_class)
