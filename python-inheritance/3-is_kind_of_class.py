c26f82e0601b44ea50708f1e33e344065202f7d2
Fiegsy <yassine.daoui031@gmail.com>
2024-02-13 03:48:10 -0800

    Fixed indt

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Function to check if an object is an instance of a class"""
+"""Function to check if an object is an instance of a class or one of its derived classes"""
 
 
-def is_instance_of_class(obj, a_class):
-    """Function that returns True if the object is an instance of the specified class"""
+def is_instance_or_subclass(obj, a_class):
+    """Check if an object is an instance of a specified class or one of its derived classes"""
     return isinstance(obj, a_class)
