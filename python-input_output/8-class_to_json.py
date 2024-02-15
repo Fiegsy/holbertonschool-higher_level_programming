32fc676c4b14cf99a43639dc6136933811dd27bd
Fiegsy <yassine.daoui031@gmail.com>
2024-02-15 13:11:52 -0800

    Fixed idt

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module with a function that returns a simple description for JSON serialization of an object"""
+"""Module with a function to convert an object to a dictionary for JSON serialization"""
 
 
 def obj_to_json(obj):
-    """Returns a dictionary description with simple data structure for the JSON serialization of 'obj'"""
+    """Returns a dictionary representation of the object suitable for JSON serialization"""
 
     return obj.__dict__
