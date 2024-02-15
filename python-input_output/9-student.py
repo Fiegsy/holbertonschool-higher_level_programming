69e92611c7989436684a6567d047a0f43e058787
Fiegsy <yassine.daoui031@gmail.com>
2024-02-15 13:18:06 -0800

    Fixed idt

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Module JSON"""
+"""module JSON to define class"""
 
 
 class Student:
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student object"""
+        """initialize a new Student object"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_dict(self):
-        """Returns a dictionary representation of the student"""
+    def to_json(self):
         return self.__dict__
