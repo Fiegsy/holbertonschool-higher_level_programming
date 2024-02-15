ab8068885cfdaf9472e2b17cbc677adfa8da5409
Fiegsy <yassine.daoui031@gmail.com>
2024-02-15 13:16:12 -0800

    Fixed idt

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,28 +1,14 @@
 #!/usr/bin/python3
-"""Module that defines the Student class"""
+"""Module JSON"""
 
 
 class Student:
-    """Defines the Student class"""
-
     def __init__(self, first_name, last_name, age):
-        """Constructor method for the Student class.
-
-        Args:
-            first_name (str): The student's first name
-            last_name (str): The student's last name
-            age (int): The student's age
-        """
-
+        """Initialize a new Student object"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_dict(self):
         """Returns a dictionary representation of the student"""
-
-        return {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "age": self.age
-        }
+        return self.__dict__
