028280e6bc5f2494eb37a22867a92736505668e5
Fiegsy <yassine.daoui031@gmail.com>
2024-02-19 14:08:03 -0800

    Fixed idt

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,68 +1,58 @@
 #!/usr/bin/python3
-"""This module defines a Base class and some related functionalities."""
+"""Module defining the Base class"""
+
 import json
-import os
+from os.path import exists
 
 
 class Base:
-    """Base class for other classes"""
+    """Defines the attributes and methods of the Base class"""
 
-    _num_instances = 0
+    __nb_objects = 0
 
-    def __init__(self, identifier=None):
-        """Initialize a Base instance."""
-        if identifier is not None:
-            self.identifier = identifier
-        else:
-            Base._num_instances += 1
-            self.identifier = Base._num_instances
+    def __init__(self, id=None):
+        """Constructor method for the Base class.
+
+        Args:
+            id (int): Positive integer to set class instance's id to.
+        """
+        self.id = id if id is not None else Base.__nb_objects + 1
+        Base.__nb_objects += 1
 
     @staticmethod
-    def to_json(data):
-        """Convert data to JSON."""
-        return json.dumps(data) if data else "[]"
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of 'list_dictionaries'"""
+        return json.dumps(list_dictionaries) if list_dictionaries else "[]"
 
     @classmethod
-    def save_to_file(cls, instances):
-        """Save instances to a JSON file."""
-        instances = instances or []
-        filename = f"{cls.__name__}_instances.json"
-        with open(filename, "w") as file:
-            data = [instance.serialize() for instance in instances]
-            file.write(cls.to_json(data))
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of 'list_objs' to a file"""
+        list_objs = list_objs or []
+        my_list = [obj.to_dictionary() for obj in list_objs]
+        filename = f"{cls.__name__}.json"
+        with open(filename, "w") as f:
+            f.write(Base.to_json_string(my_list))
 
     @staticmethod
-    def from_json(json_string):
-        """Load data from JSON string."""
+    def from_json_string(json_string):
+        """Returns the list of the JSON representation 'json_string'"""
         return json.loads(json_string) if json_string else []
 
     @classmethod
-    def create_instance(cls, **kwargs):
-        """Create an instance from dictionary."""
-        if cls.__name__ == "Rectangle":
-            instance = cls(1, 1)
-        else:
-            instance = cls(1)
-        instance.update(**kwargs)
-        return instance
+    def create(cls, **dictionary):
+        """Returns an instance with attributes already set"""
+        dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
 
     @classmethod
     def load_from_file(cls):
-        """Load instances from JSON file."""
-        filename = f"{cls.__name__}_instances.json"
-        if not os.path.exists(filename):
+        """Returns a list of instances from a file containing JSON string
+        representation of a class 'cls' object"""
+        filename = f"{cls.__name__}.json"
+        if not exists(filename):
             return []
-        with open(filename, 'r', encoding="utf-8") as file:
-            json_data = file.read()
-            data = cls.from_json(json_data)
-            instances = [cls.create_instance(**item) for item in data]
-        return instances
-
-    def serialize(self):
-        """Serialize instance to dictionary."""
-        return self.__dict__
-
-    def update(self, **kwargs):
-        """Update instance attributes."""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        with open(filename, "r") as f:
+            json_str = f.read()
+            json_list = cls.from_json_string(json_str)
+            return [cls.create(**dict) for dict in json_list]
