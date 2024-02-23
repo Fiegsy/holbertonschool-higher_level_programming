8a43259f1b57463b1d7bf123bd91580b21a6323f
Fiegsy <yassine.daoui031@gmail.com>
2024-02-23 03:37:27 -0800

    Delete file and made idt adjustements

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,57 +1,49 @@
 #!/usr/bin/python3
-"""Base test"""
-
+"""Unit tests for Base class"""
 
 import unittest
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def test_1(self):
+    def test_default_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
-        
-    def test_2(self):
+
+    def test_id_increment(self):
         b1 = Base()
-        self.assertEqual(b1.id, 2)
-        
-    def test_3(self):
+        b2 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+
+    def test_custom_id(self):
         b1 = Base(100000)
         self.assertEqual(b1.id, 100000)
 
-    def test_4(self):
+    def test_float_id(self):
         b1 = Base(1.1)
         self.assertEqual(b1.id, 1.1)
-        
-    def test_5(self):
-        Base.to_json_string([])
 
-    def test_6(self):
+    def test_to_json_string_empty(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_save_to_file_empty(self):
         Base.save_to_file([])
 
-    def test_7(self):
+    def test_save_to_file_none(self):
         Base.save_to_file(None)
 
-    def test_8(self):
-        Base.to_json_string([])
+    def test_to_json_string_list(self):
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
 
-    def test_9(self):
-        Base.to_json_string([ { 'id': 12 }])
+    def test_from_json_string_none(self):
+        self.assertIsNone(Base.from_json_string(None))
 
-    def test_10(self):
-        Base.from_json_string(None)
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
 
-    def test_11(self):
-        Base.from_json_string("[]")
-
-    def test_12(self):
-        Base.from_json_string("[]")
-
-    def test_13(self):
-        Base.from_json_string('[{ "id": 89 }]')
-
-    def test_14(self):
-        Base.from_json_string('[{ "id": 89 }]')
+    def test_from_json_string_single_item(self):
+        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
 
 if __name__ == '__main__':
     unittest.main()
