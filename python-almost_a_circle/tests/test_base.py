881e5eb72dd15b3cdf09199e4dd106c5e4a8c65d
Fiegsy <yassine.daoui031@gmail.com>
2024-02-23 03:41:43 -0800

    Fixed idt

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,49 +1,54 @@
 #!/usr/bin/python3
-"""Unit tests for Base class"""
+"""Base class unit tests"""
 
 import unittest
 from models.base import Base
 
 
 class TestBase(unittest.TestCase):
-    def test_default_id(self):
+    def test_create_base_instance_with_default_id(self):
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
-    def test_id_increment(self):
+    def test_create_base_instance_with_incremented_id(self):
         b1 = Base()
-        b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b1.id, 2)
 
-    def test_custom_id(self):
+    def test_create_base_instance_with_custom_id(self):
         b1 = Base(100000)
         self.assertEqual(b1.id, 100000)
 
-    def test_float_id(self):
+    def test_create_base_instance_with_float_id(self):
         b1 = Base(1.1)
         self.assertEqual(b1.id, 1.1)
 
-    def test_to_json_string_empty(self):
-        self.assertEqual(Base.to_json_string([]), "[]")
+    def test_to_json_string_with_empty_list(self):
+        Base.to_json_string([])
 
-    def test_save_to_file_empty(self):
+    def test_save_to_file_with_empty_list(self):
         Base.save_to_file([])
 
-    def test_save_to_file_none(self):
+    def test_save_to_file_with_None(self):
         Base.save_to_file(None)
 
-    def test_to_json_string_list(self):
-        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+    def test_to_json_string_with_list_containing_dictionary(self):
+        Base.to_json_string([{'id': 12}])
 
-    def test_from_json_string_none(self):
-        self.assertIsNone(Base.from_json_string(None))
+    def test_from_json_string_with_None(self):
+        Base.from_json_string(None)
 
-    def test_from_json_string_empty(self):
-        self.assertEqual(Base.from_json_string("[]"), [])
+    def test_from_json_string_with_empty_string(self):
+        Base.from_json_string("[]")
 
-    def test_from_json_string_single_item(self):
-        self.assertEqual(Base.from_json_string('[{"id": 89}]'), [{'id': 89}])
+    def test_from_json_string_with_empty_list(self):
+        Base.from_json_string("[]")
+
+    def test_from_json_string_with_list_containing_dictionary(self):
+        Base.from_json_string('[{"id": 89}]')
+
+    def test_from_json_string_with_list_containing_dictionary_2(self):
+        Base.from_json_string('[{"id": 89}]')
+
 
 if __name__ == '__main__':
     unittest.main()
--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -6,13 +6,13 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    def test_create_rectangle(self):
+    def test_create_rectangle_with_width_and_height(self):
         Rectangle(1, 2)
 
-    def test_create_rectangle_with_x(self):
+    def test_create_rectangle_with_width_height_and_x(self):
         Rectangle(1, 2, 3)
 
-    def test_create_rectangle_with_x_and_y(self):
+    def test_create_rectangle_with_width_height_x_and_y(self):
         Rectangle(1, 2, 3, 4)
 
     def test_create_rectangle_invalid_width_string(self):
@@ -35,7 +35,7 @@ class TestRectangle(unittest.TestCase):
             Rectangle(1, 2, 3, "4")
         self.assertTrue('y must be an integer' in str(context.exception))
 
-    def test_create_rectangle_with_extra_argument(self):
+    def test_create_rectangle_with_width_height_x_y_and_id(self):
         Rectangle(1, 2, 3, 4, 5)
 
     def test_create_rectangle_negative_width(self):
@@ -68,16 +68,16 @@ class TestRectangle(unittest.TestCase):
             Rectangle(1, 2, 3, -4)
         self.assertTrue('y must be >= 0' in str(context.exception))
 
-    def test_create_rectangle_dictionary(self):
+    def test_create_rectangle_dictionary_with_width_and_height(self):
         Rectangle.create(**{'width': 2, 'height': 3})
 
-    def test_create_rectangle_dictionary_with_x(self):
+    def test_create_rectangle_dictionary_with_width_height_and_x(self):
         Rectangle.create(**{'width': 2, 'height': 3, 'x': 12})
 
-    def test_create_rectangle_dictionary_with_x_and_y(self):
+    def test_create_rectangle_dictionary_with_width_height_x_and_y(self):
         Rectangle.create(**{'width': 2, 'height': 3, 'x': 12, 'y': 1})
 
-    def test_create_rectangle_dictionary_with_all_arguments(self):
+    def test_create_rectangle_dictionary_with_width_height_x_y_and_id(self):
         Rectangle.create(**{'width': 2, 'height': 3, 'x': 12, 'y': 1, 'id': 89})
 
     def test_load_rectangle_from_file(self):
@@ -88,46 +88,39 @@ class TestRectangle(unittest.TestCase):
         r2 = Rectangle(10, 2, 1, 3, 5)
         Rectangle.save_to_file([r1, r2])
 
-    def test_save_empty_rectangle_to_file(self):
+    def test_save_rectangle_to_file_empty_list(self):
         Rectangle.save_to_file([])
 
-    def test_save_none_rectangle_to_file(self):
+    def test_save_rectangle_to_file_with_None(self):
         dt = Rectangle(1, 2, 3, 4, 5)
         dt.save_to_file(None)
 
-    def test_to_dictionary(self):
+    def test_rectangle_to_dictionary(self):
         with self.assertRaises(AttributeError) as context:
             Rectangle.to_dictionary(self)
         self.assertTrue("'TestRectangle' object has no attribute 'width'" in str(context.exception))
 
-    def test_area(self):
+    def test_rectangle_area(self):
         dt = Rectangle(10, 10)
         dt.area()
 
-    def test_display(self):
+    def test_rectangle_display(self):
         dt = Rectangle(1, 1)
         dt.display()
 
-    def test_save_none_to_file(self):
-        Rectangle.save_to_file(None)
-
-    def test_area_calculation(self):
+    def test_rectangle_area_with_specified_values(self):
         dt = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(dt.area(), 2)
 
-    def test_to_string_representation(self):
+    def test_rectangle_string_representation(self):
         dt = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(str(dt), '[Rectangle] (5) 3/4 - 1/2')
 
-    def test_display_with_coordinates(self):
-        dt = Rectangle(1, 2, 3, 4)
-        dt.display()
-
-    def test_display_with_partial_coordinates(self):
+    def test_rectangle_display_with_specified_values(self):
         dt = Rectangle(1, 2, 3)
         dt.display()
 
-    def test_display_with_no_coordinates(self):
+    def test_rectangle_display_with_default_values(self):
         dt = Rectangle(1, 2)
         dt.display()
 
