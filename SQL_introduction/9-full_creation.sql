47d03cde6d9ed5e1ddbe6b71748d803887dc3cae
Fiegsy <yassine.daoui031@gmail.com>
2024-03-19 15:37:15 -0700

    Fixed idtention again

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS second_table (
     id int,
     name VARCHAR(256),
+    score int
 );
 INSERT INTO second_table (id, name, score) VALUE (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUE (2, 'Alex', 3);
