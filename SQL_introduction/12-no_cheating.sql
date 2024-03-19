3180839d172776c1b851481de4da9a7fcc04fcc7
Fiegsy <yassine.daoui031@gmail.com>
2024-03-19 16:10:24 -0700

    Added task 12 and 13 for test check

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Make chnages of the score of Bob from second_table to 10
+UPDATE second_table
+SET score = 10
+WHERE name = 'Bob';
---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Delete all records from second_table with a score <= 5
+DELETE FROM second_table
+WHERE score <= 5;
