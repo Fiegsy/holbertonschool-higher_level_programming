66a1f0556333dfed1bd82534788f7084da1af364
Fiegsy <yassine.daoui031@gmail.com>
2024-03-21 08:11:55 -0700

    Fixed indedention

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,13 @@
--- Make cities table into database to mysql server
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL);
-    
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    city_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    city_name VARCHAR(256) NOT NULL,
+    population INT NOT NULL,
+    state VARCHAR(100) NOT NULL
+);
