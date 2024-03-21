c5e631e9d72976380b346bc915aee598d72d3ea0
Fiegsy <yassine.daoui031@gmail.com>
2024-03-21 08:10:49 -0700

    Added file for task 8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Display all the cities of California found in the database 
+SELECT id, name FROM cities
+WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = 'California')
+ORDER BY cities.id;
