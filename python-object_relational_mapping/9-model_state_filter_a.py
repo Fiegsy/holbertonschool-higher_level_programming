0a0846dc2c8e9892b0bc217294c6aaa38448469c
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:02:53 -0700

    updtaed files with query to fileter state

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Display all State objects from the database `hbtn_0e_6_usa`.
+This script lists all State objects from the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
@@ -8,14 +8,24 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-if __name__ == "__main__":
+def list_states_with_a():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(State.name.like("%a%")):
-        print("{}: {}".format(state.id, state.name))
+    states = session.query(State).filter(State.name.like("%a%")).all()
+
+    if states:
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    else:
+        print("No states found")
 
     session.close()
+
+if __name__ == "__main__":
+    list_states_with_a()
