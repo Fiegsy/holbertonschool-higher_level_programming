dc25a44fa59952e050a4f3eaa3e789dd953c094b
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:07:15 -0700

    Fixed indetention to add print statement for display id

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Create the State object to the database `hbtn_0e_6_usa`.
+This script adds the State object “Louisiana” to the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
@@ -8,9 +8,11 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-if __name__ == "__main__":
+def add_louisiana_state():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -19,6 +21,9 @@ if __name__ == "__main__":
     session.add(new_state)
     session.commit()
 
-    print(new_state.id)
+    print("New state added with id:", new_state.id)
 
     session.close()
+
+if __name__ == "__main__":
+    add_louisiana_state()
