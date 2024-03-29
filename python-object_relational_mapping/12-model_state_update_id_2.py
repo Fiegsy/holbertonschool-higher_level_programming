342604947e799b3549b04ee71b8050091e66886a
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:12:25 -0700

    Added error handling to check if state exist

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Create the State object  to the database `hbtn_0e_6_usa`.
+This script updates the name of the State object with id=2 to "New Mexico"
+in the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
@@ -8,19 +9,24 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-if __name__ == "__main__":
+def update_state_name():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = (
-        session.query(State)
-        .filter(State.id == 2)
-        .update({State.name: "New Mexico"})
-    )
-
-    session.commit()
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+        print("State name updated successfully.")
+    else:
+        print("State with id=2 not found.")
 
     session.close()
+
+if __name__ == "__main__":
+    update_state_name()
