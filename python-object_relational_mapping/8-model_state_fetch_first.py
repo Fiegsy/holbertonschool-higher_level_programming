6ec05b66613436b3326ea78d49fe2b2876dbbc3c
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:00:43 -0700

    fixed idtention

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """
-This script lists all State objects from the database `hbtn_0e_6_usa`.
+That lists all State objects from the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.state import Base, State
+from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).all()
+    state = session.query(State).order_by(State.id).first()
 
-    if states:
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
     else:
-        print("No states found")
+        print("Nothing")
 
     session.close()
