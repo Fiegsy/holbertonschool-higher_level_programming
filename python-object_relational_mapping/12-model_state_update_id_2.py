1369daaa70396d237b1e528d333a3b063d2470f3
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:10:47 -0700

    Fixed indetention

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This script updates the name of the State object with id=2 to "New Mexico"
-in the database `hbtn_0e_6_usa`.
+Create the State object  to the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
@@ -9,24 +8,19 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-def update_state_name():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_to_update = session.query(State).filter_by(id=2).first()
-    if state_to_update:
-        state_to_update.name = "New Mexico"
-        session.commit()
-        print("State name updated successfully.")
-    else:
-        print("State with id=2 not found.")
+    new_state = (
+        session.query(State)
+        .filter(State.id == 2)
+        .update({State.name: "New Mexico"})
+    )
+
+    session.commit()
 
     session.close()
-
-if __name__ == "__main__":
-    update_state_name()
