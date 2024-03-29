4154025ffeace39f97c65c4d793e75b4b3208def
Fiegsy <yassine.daoui031@gmail.com>
2024-03-29 01:06:17 -0700

    Added print statement to display id

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script adds the State object “Louisiana” to the database `hbtn_0e_6_usa`.
+Create the State object to the database `hbtn_0e_6_usa`.
 """
 
 from sqlalchemy import create_engine
@@ -8,11 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-def add_louisiana_state():
+if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -21,9 +19,6 @@ def add_louisiana_state():
     session.add(new_state)
     session.commit()
 
-    print("New state added with id:", new_state.id)
+    print(new_state.id)
 
     session.close()
-
-if __name__ == "__main__":
-    add_louisiana_state()
